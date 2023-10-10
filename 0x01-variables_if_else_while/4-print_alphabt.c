ad74a577b4cf161568210a625ec9047e9a683be7
anassdrs <deroussi36@gmail.com>
2023-10-10 02:08:05 -0700

    commit 4.0

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -5,12 +5,12 @@
  *
  * Description: print all alphabet except q and e
  *
- * Return : Always 0 (Success)
+ * Return: Always 0 (Success)
  **/
 
 int main(void)
 {
-	chat ch = 'a';
+	char ch = 'a';
 		while (ch <= 'z')
 		{
 			if (ch == 'e' || ch == 'q')
