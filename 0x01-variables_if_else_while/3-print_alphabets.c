d67c3e8c4ea68eb0e215d50c80aaaa24c9a06e07
anassdrs <deroussi36@gmail.com>
2023-10-10 02:05:52 -0700

    3.1

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -14,13 +14,13 @@ int main(void)
 	char ch = 'a';
 	char CH = 'A';
 	/*prints a - z*/
-	while (ch < 'z')
+	while (ch <= 'z')
 	{
 		putchar(ch);
 		ch++;
 	}
 	    /*prints A - Z*/
-	while (CH < 'Z')
+	while (CH <= 'Z')
 	{
 		putchar(CH);
 		CH++;
