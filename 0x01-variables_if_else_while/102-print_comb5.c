d49104e71979d3e6dbc52206e7419533e6fd468a
anassdrs <deroussi36@gmail.com>
2023-10-13 12:41:18 -0700

    commit 102

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,23 +6,24 @@
  */
 int main(void)
 {
-	int a = '0', b;
+	int a, b;
 
-	for (; a <= 98; a++)
+	for (a = 0; a <= 98; a++)
 	{
-		for (b = a + 1; b <= 99; b++)
+		for (b = 0 ; b <= 99; b++)
 		{
-			putchar((a / 10) + '0');
-			putchar((a % 10) + '0');
-			putchar(' ');
-			putchar((b / 10) + '0');
-			putchar((b % 10) + '0');
-			if (a == 98 && b == 99)
+			if (a != b)
 			{
-				continue;
+				putchar(a / 10 + '0');
+				putchar(a % 10 + '0');
+				putchar(b / 10 + '0');
+				putchar(b % 10 + '0');
+				if (a != 98 && b != 99)
+				{
+				putchar(' ');
+				putchar(',');
+				}
 			}
-			putchar(',');
-			putchar(' ');
 		}
 	}
 	putchar('\n');
