f083fce4cfedb0c1c2d3ba8b08ffdd0d708b4177
anassdrs <deroussi36@gmail.com>
2023-10-13 13:33:10 -0700

    comit

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,32 +1,29 @@
 #include <stdio.h>
-/**
- * main - A program that prints all possible combinations
- * of two two-digit numbers.
- * Return: 0 (Sucess)
- */
-int main(void)
-{
-	int a, b;
 
-	for (a = 0; a <= 98; a++)
-	{
-		for (b = 0 ; b <= 99; b++)
-		{
-			if (a != b)
-			{
-				putchar(a / 10 + '0');
-				putchar(a % 10 + '0');
-				putchar(' ');
-				putchar(b / 10 + '0');
-				putchar(b % 10 + '0');
-				if (a != 98 && b != 99)
-				{
-				putchar(',');
-				putchar(' ');
-				}
-			}
-		}
-	}
-	putchar('\n');
-	return (0);
+int main() {
+    int tens1, ones1, tens2, ones2;
+
+    for (tens1 = 0; tens1 < 10; tens1++) {
+        for (ones1 = 0; ones1 < 10; ones1++) {
+            for (tens2 = tens1; tens2 < 10; tens2++) {
+                for (ones2 = (tens1 == tens2) ? ones1 + 1 : 0; ones2 < 10; ones2++) {
+                    putchar(tens1 + '0');
+                    putchar(ones1 + '0');
+                    putchar(' ');
+                    putchar(tens2 + '0');
+                    putchar(ones2 + '0');
+
+                    if (!(tens1 == 9 && ones1 == 9 && tens2 == 9 && ones2 == 9)) {
+                        putchar(',');
+                        putchar(' ');
+                    }
+                }
+            }
+        }
+    }
+
+    putchar('\n');
+
+    return 0;
 }
+
